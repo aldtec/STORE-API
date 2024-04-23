6e9d8e41f5a74fb7393e713ce4c0594a6c35dc07
aldtec <aldtec@outlook.com>
2024-04-23 09:04:50 -0300

    Atualização product.py
    Adicionadas as funções para busca por nome e faixa de preço.

--- store/usecases/product.py
+++ store/usecases/product.py
@@ -30,7 +30,14 @@ class ProductUsecase:
 
     async def query(self) -> List[ProductOut]:
         return [ProductOut(**item) async for item in self.collection.find()]
-
+    
+    # Coloquei sem indice mesmo pois é um projeto pequeno
+    async def query_nome(self, name: str) -> List[ProductOut]:
+        result = await self.collection.find({"name": {"$regex": f"/{name}/i"}})
+        if not result:
+            raise NotFoundException(message=f"Product not found with name: {id}")
+        return ProductOut(**result)
+ 
     async def update(self, id: UUID, body: ProductUpdate) -> ProductUpdateOut:
         result = await self.collection.find_one_and_update(
             filter={"id": id},
@@ -48,6 +55,22 @@ class ProductUsecase:
         result = await self.collection.delete_one({"id": id})
 
         return True if result.deleted_count > 0 else False
+
+    # Buscar um Produto por faixa de preço    
+    async def query_price_range(self, min_price: float = None, max_price: float = None) -> List[ProductOut]:
+        if min_price is not None and max_price is not None and min_price > max_price:
+            raise ValueError(message="Minimum price cannot be greater than maximum price.")
+
+        price_filter = {}
+        if min_price is not None:
+            price_filter["price"] = {"$gt | gte": min_price}  # Greater than or equal to min_price
+        if max_price is not None:
+            price_filter["price"] = {"$lt | lte": max_price}  # Less than or equal to max_price
+
+        cursor = self.collection.find(filter)
+        if not cursor:
+            raise NotFoundException(message=f"Products not found with this range of price from {min_price} to {max_price}")
+        return [ProductOut(**item) async for item in cursor]
 
 
 product_usecase = ProductUsecase()
